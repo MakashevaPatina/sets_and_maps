71c08541764c0fe3356438628431b1dc590d7f70
Patina Makasheva <patinamakasheva@gmail.com>
2024-06-16 12:51:01 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sets and maps");
+    }
+}
